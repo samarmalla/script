627aba566351af1805795576c239ae118fa2acfd
samarmalla <samar.malla1@gmail.com>
2025-05-14 20:01:54 -0700

    javascript basics

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+//variables
+let name = "samar";
+var age = 18;
+const student = true;
 
+console.log(typeof(name), name);
+console.log(typeof(age), age);
+console.log(typeof(student), student);
+
+// operators
+let a = 10;
+let b = 30;
+let c = a + b;
+console.log(c);
+let d = a - b;
+console.log(d);
+let e = a * b;
+console.log(e);
+let f = a / b;
+console.log(f);
+let g = a % b;
+console.log(g);
+let h = a ** b;
+console.log(h);
+let i = a++;
+console.log(i);
+let j = ++a;
+console.log(j);
+let k = a--;
+console.log(k);
+let l = --a;
+console.log(l);
+
+//compund assingment
+a+= 5;
+console.log(a);
+b-= 5;
+console.log(b);
+a*= 5;
+console.log(a);
+b/= 5;
+console.log(b);
+
+//comparison operators
+let x = 10;
+let y = 10;
+
+console.log(x == y);
+console.log(x === y); 
+console.log(x != y);
+console.log(x !== y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y); 
